33657b63701b0d742289aa427156bddf0a8e69a0
EMERALD-CU <emeraldalphanter@gmail.com>
2026-07-08 10:51:17 +0700

    feat: setup AuthService for local storage and fix dependencies

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,27 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  // Kata kunci rahasia untuk menyimpan data di memori HP
+  static const String _loginKey = 'isLoggedIn';
+  static const String _emailKey = 'userEmail';
+
+  // 1. Fungsi untuk MENYIMPAN status login (Dipanggil saat user klik "Lanjutkan/Daftar")
+  static Future<void> saveLoginStatus(bool isLogged, String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_loginKey, isLogged);
+    await prefs.setString(_emailKey, email);
+  }
+
+  // 2. Fungsi untuk MEMBACA status login (Dipanggil saat aplikasi pertama kali dibuka)
+  static Future<bool> checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_loginKey) ?? false; // Jika belum pernah login, kembalikan 'false'
+  }
+
+  // 3. Fungsi untuk MENGHAPUS status login (Dipanggil saat user klik Logout)
+  static Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_loginKey);
+    await prefs.remove(_emailKey);
+  }
+}
